8f23f11319123871bdaf6d53459149b719032a44
aravind2916 <aravind.m@gndsolutions.in>
2025-12-18 10:41:06 +0530

    Delete APITesting directory

--- APITesting/fixtures/client.py
+++
@@ -1,8 +0,0 @@
-import pytest
-import requests
-
-@pytest.fixture(scope="session")
-def api_client():
-    """Session client for all API calls."""
-    session = requests.Session()
-    return session
